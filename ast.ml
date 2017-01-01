2826dcc7a04c5b37a90c1b57a5aec9f226dac19b
Martin Keegan <martin@no.ucant.org>
2017-01-01 14:30:01 +0000

    be honest about tuples

--- ast.ml
+++ ast.ml
@@ -1,15 +1,17 @@
 
 type ident = Ident of string
 
+type tuple = (ident * string)
+
 type tag =
   | Tag of ident
   | End_tag of ident
 
 type t =
-  | Header of (ident * string)
+  | Header of tuple
   | Eof
   | Cons of (t * t)
-  | Kvp of (ident * string)
+  | Kvp of tuple
 
 let string_of_ident = function
 	| Ident s -> "$" ^ s
