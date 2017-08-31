164b3cb474871d99fdbb9469d1e0d266853ae93a
Martin Keegan <martin@no.ucant.org>
2017-08-31 02:52:49 +0100

    reindent

--- src/main.ml
+++ src/main.ml
@@ -12,102 +12,102 @@ open Ast
 exception Wrong_tag of (string * string)
 
 let main debug =
-    let debug_log s =
-      if debug
-      then print_endline s
-      else ()
-    in
+  let debug_log s =
+    if debug
+    then print_endline s
+    else ()
+  in
 
-    let assert_tag_name node expected_name =
-      let observed_name = string_of_tag node.tag_name in
-        if observed_name = expected_name
-        then ()
-        else raise (Wrong_tag (observed_name, expected_name))
-    in
+  let assert_tag_name node expected_name =
+    let observed_name = string_of_tag node.tag_name in
+      if observed_name = expected_name
+      then ()
+      else raise (Wrong_tag (observed_name, expected_name))
+  in
 
-    let transaction_of_node_contents = function
-      | [Kvp a; Kvp b; Kvp c; Kvp d; Kvp e] ->
-         Stmttrn.of_tuples a b c d e
-      | _ -> assert false
-    in
+  let transaction_of_node_contents = function
+    | [Kvp a; Kvp b; Kvp c; Kvp d; Kvp e] ->
+       Stmttrn.of_tuples a b c d e
+    | _ -> assert false
+  in
 
-    let visit_banktranlist contents_ =
-      let rec visit_banktran = function
-        | Node elt -> string_of_tag elt.tag_name |>
-            handle_banktran elt.node_contents
-        | Kvp x -> Banktranlist.parse_tuple x
-      and handle_banktran contents = function
-        | "STMTTRN" -> Banktranlist.Transaction
-           (transaction_of_node_contents contents)
-        | s -> debug_log s; assert false
-      and visit_banktranlist = function
-        | [] -> []
-        | hd :: tl -> visit_banktran hd :: visit_banktranlist tl
-      in
-        visit_banktranlist contents_ |> Banktranlist.of_contents
-    in
-
-    let rec visit_stmtr = function
-      | Node elt ->
-         (match (string_of_tag elt.tag_name) with
-           | "BANKTRANLIST" -> Some (visit_banktranlist elt.node_contents)
-           | "LEDGERBAL"
-           | "BANKACCTFROM" -> None
-           | s -> debug_log s; assert false)
-      | Kvp x -> Dump.tuple x |> debug_log ; None
-    and visit_stmtrs : node_contents list -> Banktranlist.t option list =
-      function
+  let visit_banktranlist contents_ =
+    let rec visit_banktran = function
+      | Node elt -> string_of_tag elt.tag_name |>
+          handle_banktran elt.node_contents
+      | Kvp x -> Banktranlist.parse_tuple x
+    and handle_banktran contents = function
+      | "STMTTRN" -> Banktranlist.Transaction
+         (transaction_of_node_contents contents)
+      | s -> debug_log s; assert false
+    and visit_banktranlist = function
       | [] -> []
-      | hd :: tl -> visit_stmtr hd :: visit_stmtrs tl
+      | hd :: tl -> visit_banktran hd :: visit_banktranlist tl
     in
+      visit_banktranlist contents_ |> Banktranlist.of_contents
+  in
 
-    let rec visit_stmttrnr = function
-      | Node elt ->
-         (match (string_of_tag elt.tag_name) with
-           | "STMTRS" -> visit_stmtrs elt.node_contents
-           | "STATUS" -> []
-           | s -> debug_log s; assert false
-         )
-      | Kvp x -> Dump.tuple x |> debug_log ; []
-    and visit_stmttrnrs = function
-      | [] -> []
-      | hd :: tl -> visit_stmttrnr hd :: visit_stmttrnrs tl
-    in
+  let rec visit_stmtr = function
+    | Node elt ->
+       (match (string_of_tag elt.tag_name) with
+         | "BANKTRANLIST" -> Some (visit_banktranlist elt.node_contents)
+         | "LEDGERBAL"
+         | "BANKACCTFROM" -> None
+         | s -> debug_log s; assert false)
+    | Kvp x -> Dump.tuple x |> debug_log ; None
+  and visit_stmtrs : node_contents list -> Banktranlist.t option list =
+    function
+    | [] -> []
+    | hd :: tl -> visit_stmtr hd :: visit_stmtrs tl
+  in
 
-    let visit_bankmsgsrv1 = function
-      | [Node elt] ->
-         assert_tag_name elt "STMTTRNRS";
-        visit_stmttrnrs elt.node_contents
-      | _ -> assert false
-    in
+  let rec visit_stmttrnr = function
+    | Node elt ->
+       (match (string_of_tag elt.tag_name) with
+         | "STMTRS" -> visit_stmtrs elt.node_contents
+         | "STATUS" -> []
+         | s -> debug_log s; assert false
+       )
+    | Kvp x -> Dump.tuple x |> debug_log ; []
+  and visit_stmttrnrs = function
+    | [] -> []
+    | hd :: tl -> visit_stmttrnr hd :: visit_stmttrnrs tl
+  in
 
-    let visit_ofx = function
-      | [Node discard; Node bankmsgsrv1;] ->
-         assert_tag_name bankmsgsrv1 "BANKMSGSRSV1";
-        visit_bankmsgsrv1 bankmsgsrv1.node_contents
-      | _ -> assert false
-    in
+  let visit_bankmsgsrv1 = function
+    | [Node elt] ->
+       assert_tag_name elt "STMTTRNRS";
+      visit_stmttrnrs elt.node_contents
+    | _ -> assert false
+  in
 
-    let visit_top = function
-      | Root_node n ->
-         assert (string_of_tag n.tag_name = "OFX");
-         visit_ofx n.node_contents
-      | _ -> assert false;
-    in
+  let visit_ofx = function
+    | [Node discard; Node bankmsgsrv1;] ->
+       assert_tag_name bankmsgsrv1 "BANKMSGSRSV1";
+      visit_bankmsgsrv1 bankmsgsrv1.node_contents
+    | _ -> assert false
+  in
 
-    let dump_node_contents = function
-      | _ -> debug_log "OOK"
-    in
+  let visit_top = function
+    | Root_node n ->
+       assert (string_of_tag n.tag_name = "OFX");
+       visit_ofx n.node_contents
+    | _ -> assert false;
+  in
 
-      try
-        let headers = Parse_ofx.parse Lexer.header_token debug in
-        let nodes = Parse_ofx.parse Lexer.token debug in
-          ignore headers;
-          (* Dump.registry headers |> debug_log;
-          Dump.registry nodes |> debug_log; *)
-          visit_top nodes |> List.iter dump_node_contents
-      with Wrong_tag (o, e) ->
-        ("Observed: " ^ o ^ "; expected: " ^ e) |> print_endline
+  let dump_node_contents = function
+    | _ -> debug_log "OOK"
+  in
+
+    try
+      let headers = Parse_ofx.parse Lexer.header_token debug in
+      let nodes = Parse_ofx.parse Lexer.token debug in
+        ignore headers;
+        (* Dump.registry headers |> debug_log;
+        Dump.registry nodes |> debug_log; *)
+        visit_top nodes |> List.iter dump_node_contents
+    with Wrong_tag (o, e) ->
+      ("Observed: " ^ o ^ "; expected: " ^ e) |> print_endline
 
 let _ =
   let debug = ref false in
