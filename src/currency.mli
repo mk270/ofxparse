76c0148e106703cd0af5243b2018cb3a95af96a7
Martin Keegan <martin@no.ucant.org>
2017-09-04 18:56:12 +0100

    specify interface

---
+++ src/currency.mli
@@ -0,0 +1,13 @@
+(*
+  ofxparse, a parser for OFX files, by Martin Keegan
+
+  Copyright (C) 2017  Martin Keegan
+
+  This programme is free software; you may redistribute and/or modify
+  it under the terms of the Apache Licence v2.0.
+*)
+
+type t
+
+val of_string : string -> t
+val to_string : t -> string
