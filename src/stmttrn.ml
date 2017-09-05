b9f72df0144b2e25d065b1241894cdbba5b40233
Martin Keegan <martin@no.ucant.org>
2017-09-05 06:31:34 +0100

    make trntype a type

--- src/stmttrn.ml
+++ src/stmttrn.ml
@@ -7,8 +7,17 @@
   it under the terms of the Apache Licence v2.0.
 *)
 
+type trntype =
+  | DIRECTDEBIT
+  | PAYMENT
+  | OTHER
+  | REPEATPMT
+  | DIRECTDEP
+  | CASH
+  | MATCHFAILURE
+
 type t = {
-  trntype : string;
+  trntype : trntype;
   dtposted : Timestamp.t;
   trnamt : Currency.t;
   fitid : string;
@@ -17,32 +26,43 @@ type t = {
 
 exception Key_not_found of string * string
 
-let get_string expected_key = function
+let trntype_of_string = function
+  | "DIRECTDEBIT" -> DIRECTDEBIT
+  | "PAYMENT" -> PAYMENT
+  | "OTHER" -> OTHER
+  | "REPEATPMT" -> REPEATPMT
+  | "DIRECTDEP" -> DIRECTDEP
+  | "CASH" -> CASH
+  | _ -> MATCHFAILURE
+
+let string_of_trntype = function
+  | DIRECTDEBIT -> "DIRECTDEBIT"
+  | PAYMENT -> "PAYMENT"
+  | OTHER -> "OTHER"
+  | REPEATPMT -> "REPEATPMT"
+  | DIRECTDEP -> "DIRECTDEP"
+  | CASH -> "CASH"
+  | _ -> assert false
+
+let get_string' expected_key = function
   | (ident, s) -> let id = Ast.string_of_ident ident
                   in
                     if id = expected_key
                     then s
                     else raise (Key_not_found (id, expected_key))
 
-let get_trntype kvp = get_string "TRNTYPE" kvp  |> String.trim
-let get_name kvp    = get_string "NAME" kvp     |> String.trim
-let get_fitid kvp   = get_string "FITID" kvp    |> String.trim
-let get_trnamt kvp  = get_string "TRNAMT" kvp   |> String.trim
-let get_date kvp    = get_string "DTPOSTED" kvp |> String.trim |>
-                        Timestamp.of_string
+let get_string expected_key kvp =
+  get_string' expected_key kvp |> String.trim
+
+let get_name kvp    = get_string "NAME" kvp
+let get_fitid kvp   = get_string "FITID" kvp
+let get_trnamt kvp  = get_string "TRNAMT" kvp
+let get_trntype kvp = get_string "TRNTYPE" kvp |> trntype_of_string
+let get_date kvp    = get_string "DTPOSTED" kvp |> Timestamp.of_string
 
 let of_tuples a b c d e =
-  let trntype = get_trntype a in
-    (match trntype with
-     | "DIRECTDEBIT"
-     | "PAYMENT"
-     | "OTHER"
-     | "REPEATPMT"
-     | "DIRECTDEP"
-     | "CASH" -> ()
-     | _ -> print_endline trntype; assert false);
     {
-      trntype = trntype;
+      trntype = get_trntype a;
       dtposted = get_date b;
       trnamt = get_trnamt c |> Currency.of_string;
       fitid = get_fitid d;
@@ -56,7 +76,7 @@ let string_of_stmttrn trn =
     "] ";
     Timestamp.to_string trn.dtposted;
     " ";
-    trn.trntype;
+    string_of_trntype trn.trntype;
     ": ";
     Currency.to_string trn.trnamt;
     " :: ";
