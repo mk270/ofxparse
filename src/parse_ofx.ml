b1bbf7cbc2292a301793953bf8db1258f48b7605
Martin Keegan <martin@no.ucant.org>
2017-09-05 19:20:54 +0100

    reindent

--- src/parse_ofx.ml
+++ src/parse_ofx.ml
@@ -10,23 +10,22 @@
 open Ast
 
 let report_lex lexbuf =
-    let pos = lexbuf.Lexing.lex_curr_p in
-    let lnum = pos.Lexing.pos_lnum in
-    let cnum = pos.Lexing.pos_cnum in
-    let lexeme = Lexing.lexeme lexbuf in
-    Printf.printf "line: %d, byte: %d, `%s'\n"
-        lnum cnum lexeme
+  let pos = lexbuf.Lexing.lex_curr_p in
+  let lnum = pos.Lexing.pos_lnum in
+  let cnum = pos.Lexing.pos_cnum in
+  let lexeme = Lexing.lexeme lexbuf in
+    Printf.printf "line: %d, byte: %d, `%s'\n" lnum cnum lexeme
 
 let parse lx lexbuf debug =
-    try Parser.input lx lexbuf
-    with
-    | Failure f ->
-        report_lex lexbuf;
-        print_endline f;
-        failwith f
-    | Parsing.Parse_error ->
-        report_lex lexbuf;
-        failwith "parse error"
-    | Lexer.Unexpected_token ->
-        report_lex lexbuf;
-        failwith "lexer error"
+  try Parser.input lx lexbuf
+  with
+  | Failure f ->
+      report_lex lexbuf;
+      print_endline f;
+      failwith f
+  | Parsing.Parse_error ->
+      report_lex lexbuf;
+      failwith "parse error"
+  | Lexer.Unexpected_token ->
+      report_lex lexbuf;
+      failwith "lexer error"
